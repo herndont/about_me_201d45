015e78b59068dee1ca94ea5009f0c9d707ef40b8
herndont <herndontrey@gmail.com>
2019-02-06 23:39:54 -0600

    all questions have been added

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ Creating a game that asks different facts about the creator.
 
 console.log('The first question is just trying to get a name.')
 
-var username, trey, ltrey, lltrey, meet, lmeet, llmeet, sports, lsports, llsports, plane, lplane, llplane, sponsored, lsponsored, llsponsored, origin, lorigin, llorigin
+var username, trey, ltrey, meet, lmeet, sports, lsports, plane, lplane, sponsored, lsponsored, origin, lorigin
 
 username = prompt('what is your name?');
 
@@ -26,51 +26,31 @@ else {
 
 trey = prompt('Question #1: Do you know Trey?');
 ltrey = trey.toLowerCase();
-lltrey = trey.toLowerCase();
-    console.log(ltrey)
-    console.log(lltrey)
+    console.log('Question #1 answer: ' + ltrey);
 
 if(trey === ''){
     trey = prompt('Not sure why you just won\'t say Yes or No to the question!?');
 }  
 
-if(ltrey === 'yes'){
+if(ltrey === 'yes' || ltrey === 'y'){
     alert ('Lies!!! You have never met him before in your life.');
 }  
 
-if(ltrey === 'no'){
+if(ltrey === 'no' || ltrey === 'n'){
     alert ('You two should be introduced. He is really cool.');
-}
-
-if(lltrey === 'y'){
-    alert ('Lies!!! You have never met him before in your life.')
-}
-
-if (lltrey === 'n'){
-    alert ('You two should be introduced. He is really cool.')
 }
 
 /* The second game quesiton starts here*/
 
 meet = prompt('Question #2: Would you like to meet Trey?');
 lmeet = meet.toLowerCase();
-llmeet = meet.toLowerCase();
-    console.log(lmeet)
-    console.log(llmeet)
+    console.log('Question #2 answer: ' + lmeet);
 
-if(lmeet === 'yes'){
+if(lmeet === 'yes' || lmeet === 'y'){
     alert ('Great answer! He is a pretty cool guy.');
 }
 
-if(lmeet === 'no'){
-    alert ('Oh well, your loss.')
-}
-
-if(llmeet === 'y'){
-    alert ('Great answer! He is a pretty cool guy.');
-}
-
-if(llmeet ==='n'){
+if(lmeet === 'no' || lmeet === 'n'){
     alert ('Oh well, your loss.')
 }
 
@@ -78,23 +58,13 @@ if(llmeet ==='n'){
 
 sports = prompt('Question #3: Does Trey like sports?');
 lsports = sports.toLowerCase();
-llsports = sports.toLowerCase();
-    console.log(lsports)
-    console.log(llsports)
+    console.log('Question #3 answer: ' + lsports);
 
-if(lsports === 'yes'){
+if(lsports === 'yes' || lsports === 'y'){
     alert ('Great answer! Trey\'s favorite sports are sailing, soccer, skiing, and shooting.');
 }
 
-if(lsports === 'no'){
-    alert ('You are incorrect. I think you need to learn some more about Trey.')
-}
-
-if(llsports === 'y'){
-    alert ('Great answer! Trey\'s favorite sports are sailing, soccer, skiing, and shooting.')
-}
-
-if(llsports === 'n'){
+if(lsports === 'no' || lsports === 'n'){
     alert ('You are incorrect. I think you need to learn some more about Trey.')
 }
 
@@ -102,23 +72,13 @@ if(llsports === 'n'){
 
 plane = prompt('Question #4: Would Trey jump out of an airplane?');
 lplane = plane.toLowerCase();
-llplane = plane.toLowerCase();
-    console.log(lplane)
-    console.log(llplane)
+    console.log('Question #4 answer: ' + lplane);
 
-if(lplane === 'yes'){
+if(lplane === 'yes' || lplane === 'y'){
     alert ('You are right! Trey has jumped out of airplanes a lot in the Army. Don\'t tell anyone, but he is super scared of heights.');
 }
 
-if(lplane === 'no'){
-    alert ('Wrong!!! Trey is scared of heights, but has jumped out of planes quite a few times in the Army.');
-}
-
-if(llplane === 'y'){
-    alert ('You are right! Trey has jumped out of airplanes a lot in the Army. Don\'t tell anyone, but he is super scared of heights.');
-}
-
-if(llplane === 'n'){
+if(lplane === 'no' || lplane === 'n'){
     alert ('Wrong!!! Trey is scared of heights, but has jumped out of planes quite a few times in the Army.');
 }
 
@@ -126,36 +86,28 @@ if(llplane === 'n'){
 
 sponsored = prompt('Question #5: Was Trey ever a sponsored athlete?');
 lsponsored = sponsored.toLowerCase();
-llsponsored = sponsored.toLocaleLowerCase();
-    console.log(lsponsored)
-    console.log(llsponsored)
+    console.log('Question #5 answer: ' + lsponsored);
 
-if(lsponsored === 'yes'){
-    alert ('Trey was a sponsored athlete! He was a member of a division I rifle team in college and competed nationally and internaitonally in competition.')
+if(lsponsored === 'yes' || lsponsored === 'y'){
+    alert ('Trey was a sponsored athlete! He was a member of a division I rifle team in college and competed nationally and internaitonally in competition.');
 }
 
-if(lsponsored === 'no'){
-    alert ('Wrong, Trey was a sponsored member of a competitive rifle team throughout highschool and college. Sponsorship was given by ammunition companies.')
+if(lsponsored === 'no' || lsponsored === 'n'){
+    alert ('Wrong, Trey was a sponsored member of a competitive rifle team throughout highschool and college. Sponsorship was given by ammunition companies.');
 }
 
-if(llsponsored === 'y'){
-    alert ('Trey was a sponsored athlete! He was a member of a division I rifle team in college and competed nationally and internaitonally in competition.')
+/* The sixth game question starts here*/
+
+origin = prompt('Question #6: Is Trey from the West Coast?');
+lorigin = origin.toLowerCase();
+    console.log('Question #6 answer: ' + lorigin);
+
+if(lorigin === 'yes' || lorigin === 'y'){
+    alert ('Nope, Trey is from a rural farming town in central Illinois');
 }
 
-if(llsponsored === 'n'){
-    alert ('Wrong, Trey was a sponsored member of a competitive rifle team throughout highschool and college. Sponsorship was given by ammunition companies.')
+if(lorigin === 'no' || lorigin === 'n'){
+    alert ('Correct! Trey is from central Illinois.');
 }
 
 
-
-/*
-
-quirkyfact = prompt('tell me a quirky fact about yourself');
-
-if(quirkyfact === 'dnd'){
-  alert('that is my favorite thing too');
-} else {
-  alert('oh, ' + quirkyfact + ' sounds really cool, tell me more');
-}
-
-*/
