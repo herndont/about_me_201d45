d3b906d215010657e74eb568e15ee857a573ce9d
herndont <herndontrey@gmail.com>
2019-02-10 17:49:23 -0600

    new html and css files with finished lab 3 work

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@ Creating a game that asks different facts about the creator.
 
 console.log('The first question is just trying to get a name.');
 
-var username, trey, ltrey, meet, lmeet, sports, lsports, plane, lplane, sponsored, lsponsored, origin, lorigin, states, lstates, name_of_states;
+var username, trey, ltrey, meet, lmeet, sports, lsports, plane, lplane, sponsored, lsponsored, states, lstates, name_of_states, towns, towns;
+
+var score = 0;
 
 username = prompt('what is your name?');
 
@@ -37,6 +39,7 @@ if(ltrey === 'yes' || ltrey === 'y'){
 
 if(ltrey === 'no' || ltrey === 'n'){
     alert ('You two should be introduced. He is really cool.');
+    score++;
 }
 
 /* The second game quesiton starts here*/
@@ -47,6 +50,7 @@ lmeet = meet.toLowerCase();
 
 if(lmeet === 'yes' || lmeet === 'y'){
     alert ('Great answer! He is a pretty cool guy.');
+    score++;
 }
 
 if(lmeet === 'no' || lmeet === 'n'){
@@ -61,6 +65,7 @@ lsports = sports.toLowerCase();
 
 if(lsports === 'yes' || lsports === 'y'){
     alert ('Great answer! Trey\'s favorite sports are sailing, soccer, skiing, and shooting.');
+    score++;
 }
 
 if(lsports === 'no' || lsports === 'n'){
@@ -75,6 +80,7 @@ lplane = plane.toLowerCase();
 
 if(lplane === 'yes' || lplane === 'y'){
     alert ('You are right! Trey has jumped out of airplanes a lot in the Army. Don\'t tell anyone, but he is super scared of heights.');
+    score++;
 }
 
 if(lplane === 'no' || lplane === 'n'){
@@ -89,43 +95,68 @@ lsponsored = sponsored.toLowerCase();
 
 if(lsponsored === 'yes' || lsponsored === 'y'){
     alert ('Trey was a sponsored athlete! He was a member of a division I rifle team in college and competed nationally and internaitonally in competition.');
+    score++;
 }
 
 if(lsponsored === 'no' || lsponsored === 'n'){
     alert ('Wrong, Trey was a sponsored member of a competitive rifle team throughout highschool and college. Sponsorship was given by ammunition companies.');
 }
 
-/* The sixth game question starts here*/
+/* The Sixth game question starts here*/
 
-origin = prompt('Question #6: Is Trey from the West Coast?');
-lorigin = origin.toLowerCase();
-    console.log('Question #6 answer: ' + lorigin);
+var tries = 4;
+var number_to_guess = 4;
 
-if(lorigin === 'yes' || lorigin === 'y'){
-    alert ('Nope, Trey is from a rural farming town in central Illinois');
+    console.log('Question #6 answer: ' + number_to_guess);
+
+while (tries > 0){
+    towns = prompt('Question #6: How many different towns has Trey lived in while in Washington?');
+    console.log(towns);
+    if(towns < number_to_guess){
+        alert('You are incorrect. That is not enough places.');
+    } else if(towns > number_to_guess){
+        alert('You are incorrect. Thats is too many different places.');
+    } else {
+        alert('You are correct! I have lived in Lakewood, Tacoma, Auburn, and Seattle.');
+        score++;
+        break;
+    }
+    tries--;
+    console.log(tries);
 }
-
-if(lorigin === 'no' || lorigin === 'n'){
-    alert ('Correct! Trey is from central Illinois.');
+if (tries === 0){
+    alert('You have taken way too long to guess this answer. It\'s 4 silly. Lakehood, Tacoma, Auburn, and Seattle are all places I have lived.');
 }
 
 /* The Seventh game question starts here*/
 
-states = prompt('Question #7: Name a state that Trey has lived in other than Washington?');
-name_of_states = ['Illinois', 'South Carolina', 'Virginia'];
-
+name_of_states = ['illinois', 'south carolina', 'virginia'];
 var tries = 7;
+
     console.log('Question #7 answer: ' + name_of_states);
 
-for (var index = 0; index < name_of_states.length; index++) { 
-    if(states === name_of_states[index]){
-        alert ('Correct, Trey has lived in ' + states);
-    break;
+var correct_state = false;
+
+while (tries > 0){
+    states = prompt('Question #7: Name a state that Trey has lived in other than Washington?');
+    lstates=states.toLowerCase();
+    for (var index = 0; index < name_of_states.length; index++) { 
+        if(lstates === name_of_states[index]){
+            alert ('Correct, Trey has lived in ' + states);
+            correct_state = true;
+            tries=0;
+            score++;
+            break;
+        }
     }
-} 
+    if (correct_state === false){
+        alert( 'Sorry, Trey never lived in ' + states + ' state.');
+    }
+    tries--;
+  }
+    if (correct_state === false){
+        alert('Nice try, but no Cigar. I have lived in Illinois, Virginia, and South Carolina before moving to Washington.');
+    }
 
-    if(states !== name_of_states){
-        alert ('Sorry, Trey never lived in ' + states + ' state.');
-}
-
+    alert('You have gotten ' + score +' answers right. Thanks for learning a little bit about me.')
 
