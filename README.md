476309444d896dd1539f7eef33956cfcdda8947b
herndont <herndontrey@gmail.com>
2019-02-10 19:24:35 -0600

    functions added for lab 4 completion

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # about_me_201d45
+
+Getting started
+
+This site should have no issues running on any web browser. The only issue that it will have right now is accomodating to different screen sizes since the elements are in absolute formatting. This is something that will be continually updated over time.
+
+
+Pair programming
+
+As of 2/10/19 Trey Herndon is the only person to have worked on these files. Updates by date will be put below this line for a full historical account of who was working in this code base and what they did to it.
+
+
+Images
+
+There are multiple images on this page and I want to make sure that I give credit where credit is due.
+Images are listed below with citations.
+
+absract_yellow_purple_smoke_black_background.jpg        https://www.pexels.com/photo/photography-of-colorful-smoke-1065714/
+
+abstract-red-blue-smoke-background        https://www.pexels.com/photo/multicolored-smoke-1020315/
+
+logo.jpg        Created and owned by Trey Herndon
+
+afrika.jpg      Created and owned by Trey Herndon
+
+race.jpg        Created and owned by Trey Herndon
+
+summit.jpg      Created and owned by Trey Herndon
+
+
+Built with
+
+JavaScript, html5, css
+
+
+Author
+
+Trey Herndon        using VSCode
+
--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ var username, trey, ltrey, meet, lmeet, sports, lsports, plane, lplane, sponsore
 
 var score = 0;
 
+function introduction(){
 username = prompt('what is your name?');
 
 if(username === ''){
@@ -22,9 +23,13 @@ if(username === '') {
 else {
   alert ('hello ' + username + ' , it is nice to meet you.');
 }
+}
+introduction();
 
 /* The first game question is the start of the game. */
 
+
+function first_question(){
 trey = prompt('Question #1: Do you know Trey?');
 ltrey = trey.toLowerCase();
     console.log('Question #1 answer: ' + ltrey);
@@ -41,9 +46,12 @@ if(ltrey === 'no' || ltrey === 'n'){
     alert ('You two should be introduced. He is really cool.');
     score++;
 }
+}
+first_question();
 
 /* The second game quesiton starts here*/
 
+function second_question(){
 meet = prompt('Question #2: Would you like to meet Trey?');
 lmeet = meet.toLowerCase();
     console.log('Question #2 answer: ' + lmeet);
@@ -56,9 +64,12 @@ if(lmeet === 'yes' || lmeet === 'y'){
 if(lmeet === 'no' || lmeet === 'n'){
     alert ('Oh well, your loss.')
 }
+}
+second_question();
 
 /* The third game question starts here*/
 
+function third_question(){
 sports = prompt('Question #3: Does Trey like sports?');
 lsports = sports.toLowerCase();
     console.log('Question #3 answer: ' + lsports);
@@ -71,9 +82,12 @@ if(lsports === 'yes' || lsports === 'y'){
 if(lsports === 'no' || lsports === 'n'){
     alert ('You are incorrect. I think you need to learn some more about Trey.')
 }
+}
+third_question()
 
 /* The fourth game question starts here*/
 
+function fourth_question(){
 plane = prompt('Question #4: Would Trey jump out of an airplane?');
 lplane = plane.toLowerCase();
     console.log('Question #4 answer: ' + lplane);
@@ -86,9 +100,12 @@ if(lplane === 'yes' || lplane === 'y'){
 if(lplane === 'no' || lplane === 'n'){
     alert ('Wrong!!! Trey is scared of heights, but has jumped out of planes quite a few times in the Army.');
 }
+}
+fourth_question();
 
 /* The fifth game question starts here*/
 
+function fifth_question(){
 sponsored = prompt('Question #5: Was Trey ever a sponsored athlete?');
 lsponsored = sponsored.toLowerCase();
     console.log('Question #5 answer: ' + lsponsored);
@@ -101,9 +118,12 @@ if(lsponsored === 'yes' || lsponsored === 'y'){
 if(lsponsored === 'no' || lsponsored === 'n'){
     alert ('Wrong, Trey was a sponsored member of a competitive rifle team throughout highschool and college. Sponsorship was given by ammunition companies.');
 }
+}
+fifth_question();
 
 /* The Sixth game question starts here*/
 
+function sixth_question(){
 var tries = 4;
 var number_to_guess = 4;
 
@@ -127,9 +147,12 @@ while (tries > 0){
 if (tries === 0){
     alert('You have taken way too long to guess this answer. It\'s 4 silly. Lakehood, Tacoma, Auburn, and Seattle are all places I have lived.');
 }
+}
+sixth_question();
 
 /* The Seventh game question starts here*/
 
+function seventh_question(){
 name_of_states = ['illinois', 'south carolina', 'virginia'];
 var tries = 7;
 
@@ -157,6 +180,8 @@ while (tries > 0){
     if (correct_state === false){
         alert('Nice try, but no Cigar. I have lived in Illinois, Virginia, and South Carolina before moving to Washington.');
     }
+}
+seventh_question();
 
     alert('You have gotten ' + score +' answers right. Thanks for learning a little bit about me.')
 
